9016b579416f0def291108dccda4c8588463431d
milton200727 <estuardosantos2007@gmail.com>
2026-04-08 18:29:07 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+
+  // Las listas de palabras para construir la excusa
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed on', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising'];
+
+  // Elegimos un elemento al azar de cada lista
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  // Juntamos todas las partes en una sola frase
+  let excuse = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+
+  // Ponemos la excusa en el HTML
+  document.getElementById("excuse").innerHTML = excuse;
+
 };
